1ce170548f72df96593ee2ff81a80f801be18168
Isa Giongo <isagiongo@gmail.com>
2019-05-01 10:14:58 -0300

    Adiciona componente detalhes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -9,6 +9,6 @@ export const HEROES: Hero [] = [
     { id: 16, name: 'RubberMan' },
     { id: 17, name: 'Dynama' },
     { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
+    { id: 19, name: 'Vovó JuJu' },
     { id: 20, name: 'Tornado' }
 ];
